81569cd79cc702ce78bf3acdb5ee1483044d7231
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-11-02 07:07:17 +0200

    Initial styling and text input

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { Focus } from './src/features/Focus';
+import { colors } from './src/utils/colors';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Focus />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.darkBlue,
   },
 });
---
+++ src/features/Focus.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { colors } from "../utils/colors";
+import { TextInput } from "react-native-paper";
+
+export const Focus = () => {
+  <View style={styles.container}>
+    <View style={styles.inputContainer}>
+      <TextInput label="What would you like to focus on?" />
+    </View>
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 0.5,
+    padding: 25,
+    justifyContent: 'top'
+  }
+});
