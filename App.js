e1a49f651e352695e1a91d90c48f2f8de79bbccd
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-11-03 04:20:17 +0200

    adding custom button and handling user input

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
-import { Focus } from './src/features/Focus';
-import { colors } from './src/utils/colors';
+import React, { useState } from "react";
+import { StyleSheet, SafeAreaView, Platform, StatusBar } from "react-native";
+import { Focus } from "./src/features/Focus";
+import { colors } from "./src/utils/colors";
 
 export default function App() {
+  const [currentSubject, setCurrentSubject] = useState(null);
+
   return (
     <SafeAreaView style={styles.container}>
-      <Focus />
+      {!currentSubject ? (
+        <Focus addSubject={setCurrentSubject} />
+      ) : (
+        <View>
+          <Text style={{ color: colors.white }}>
+            I am going to render the timer {currentSubject}
+          </Text>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     flex: 1,
     backgroundColor: colors.darkBlue,
   },
--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,23 +1,46 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
 import { colors } from "../utils/colors";
 import { TextInput } from "react-native-paper";
+import { RoundedButton } from "../components/RoundedButton";
 
-export const Focus = () => {
-  <View style={styles.container}>
-    <View style={styles.inputContainer}>
-      <TextInput label="What would you like to focus on?" />
+export const Focus = ({ addSubject }) => {
+  const [subject, setSubject] = useState(null);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setSubject}
+          label="What would you like to focus on?"
+        />
+        <View style={styles.button}>
+          <RoundedButton
+            title="+"
+            size={50}
+            onPress={() => addSubject(subject)}
+          />
+        </View>
+      </View>
     </View>
-  </View>;
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  button: {
+    justifyContent: "center",
+  },
+  textInput: {
+    flex: 1,
+    marginRight: 10,
+  },
   inputContainer: {
-    flex: 0.5,
     padding: 25,
-    justifyContent: 'top'
-  }
+    justifyContent: "top",
+    flexDirection: "row",
+  },
 });
